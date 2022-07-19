bb205ce8fcc6267f19becf57e6f8eef82ad5ac8e
Marni Hotchkiss <90331251+mhotch24@users.noreply.github.com>
2022-07-19 10:23:07 -0400

    Update LandingPage.js

--- assets/client/src/pages/LandingPage.js
+++ assets/client/src/pages/LandingPage.js
@@ -12,14 +12,21 @@ export const LandingPage = ({isArchived}) => {
   const { apiUrl } = useContext(ApiUrlContext)
   const challengesPath = isArchived ? "/api/challenges?archived=true" : "/api/challenges"
 
-  
-  
-  
-  
-  
-  
-  
-  
+  // TODO: Temporary showing of layout on chal details until the layout is moved
+  if (isArchived) {
+    $(".top-banner").hide()
+    $(".usa-hero").hide()
+    $(".video").hide()
+    $(".newsletter").hide()
+    $(".help-section").hide()
+    $(".challenges-header").hide()
+  } else {
+    $(".top-banner").show()
+    
+  }
+  $(".help-section").show()
+  $(".section-divider").show()
+  $(".footer").show()
 
   useEffect(() => {
     let yearFilter = isArchived ? `&filter[year]=${selectedYear}` : ""
